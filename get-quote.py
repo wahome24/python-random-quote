875281063c29b3b53aa7cc0752b0916b60094e0c
Erastus Gacheru <erastusderano@gmail.com>
2020-11-30 07:24:48 +0300

    Random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -1,8 +1,11 @@
+import random
+result =random.randint(0,13)
+
 def trial():
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
-  print(quotes[0])
+  print(quotes[result])
 
 trial()
